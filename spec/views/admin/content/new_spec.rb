8032e3b15d6c0b1794b2b5aa6b7b9bec23d6ad96
Ubuntu94 <mohsinvindhani@gmail.com>
2013-09-25 17:06:10 +0530

    first commit

---
+++ spec/views/admin/content/new_spec.rb
@@ -0,0 +1,74 @@
+require 'spec_helper'
+
+describe "admin/content/new.html.erb" do
+  before do
+    admin = stub_model(User, :settings => {:editor => 'simple'}, :admin? => true,
+                       :text_filter_name => "", :profile_label => "admin")
+    blog = mock_model(Blog, :base_url => "http://myblog.net/")
+    article = stub_model(Article).as_new_record
+    text_filter = stub_model(TextFilter)
+
+    article.stub(:text_filter) { text_filter }
+    view.stub(:current_user) { admin }
+    view.stub(:this_blog) { blog }
+
+    # FIXME: Nasty. Controller should pass in @categories and @textfilters.
+    Category.stub(:all) { [] }
+    TextFilter.stub(:all) { [text_filter] }
+
+    assign :article, article
+  end
+
+  it "renders with no resources or macros" do
+    assign(:images, [])
+    assign(:macros, [])
+    assign(:resources, [])
+    render
+  end
+
+  it "renders with image resources" do
+    # FIXME: Nasty. Thumbnail creation should not be controlled by the view.
+    img = mock_model(Resource, :filename => "foo", :create_thumbnail => nil)
+    assign(:images, [img])
+    assign(:macros, [])
+    assign(:resources, [])
+    render
+  end
+
+  it "let admin see a merge button" do
+    assign(:images, [])
+    assign(:macros, [])
+    assign(:resources, [])
+    render
+    rendered.should contain "Merge"
+  end
+end
+
+describe "admin/content/new.html.erb" do
+ before do
+    user = stub_model(User, :settings => {:editor => 'simple'}, :admin? => false,
+                       :text_filter_name => "user", :profile_label => "user")
+
+    blog = mock_model(Blog, :base_url => "http://myblog.net/")
+    article = stub_model(Article).as_new_record
+    text_filter = stub_model(TextFilter)
+
+    article.stub(:text_filter) { text_filter }
+    view.stub(:current_user) { user }
+    view.stub(:this_blog) { blog }
+
+    # FIXME: Nasty. Controller should pass in @categories and @textfilters.
+    Category.stub(:all) { [] }
+    TextFilter.stub(:all) { [text_filter] }
+
+    assign :article, article
+  end
+
+  it "let user see no merge button" do
+    assign(:images, [])
+    assign(:macros, [])
+    assign(:resources, [])
+    render
+    rendered.should_not contain "Merge"
+  end
+end
